8c5db296c79708191adee94981d1defba8657765
Jackson Gray <nohaxjustdoge@gmail.com>
2024-10-22 02:38:09 +0000

    Update import path for Search component from LocationService

--- app/examples/index.tsx
+++ app/examples/index.tsx
@@ -1,6 +1,6 @@
 import { json, redirect } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
-import { Search } from "~/components/LocationService";
+import { Search } from "~/components/OldLocationService";
 import { Details } from "~/components/Details";
 
 // Mock external API call
