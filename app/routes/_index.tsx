3b73deb1d9e0e768d0e50caaff26aef56a13fc29
Jackson Gray <nohaxjustdoge@gmail.com>
2024-10-23 00:19:20 +0000

    Refactor location handling and add city to geocoding response

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -29,34 +29,40 @@ export const action: ActionFunction = async ({ request, context }) => {
     const parsedData = JSON.parse(locationDataString);
     if (isLocationData(parsedData)) {
       const locationData: LocationData = parsedData;
-      // Process the locationData here
-      if (locationData.type === "geolocation") {
-        return redirect(
-          `/?lat=${encodeURIComponent(
+      let geocodingUrl = "";
+      const apiKey = context.cloudflare.env.GOOGLE_MAPS_API_KEY;
+      switch (locationData.type) {
+        case "geolocation":
+          geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
             locationData.data.coords.latitude
-          )}&lon=${encodeURIComponent(locationData.data.coords.longitude)}`
-        );
-      } else if (locationData.type === "input") {
-        try {
-          const apiKey = context.cloudflare.env.GOOGLE_MAPS_API_KEY;
-          const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+          )},${encodeURIComponent(
+            locationData.data.coords.longitude
+          )}&key=${apiKey}`;
+          break;
+        case "input":
+          geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
             locationData.data
           )}&key=${apiKey}`;
-          console.log(geocodingUrl)
-          const response = await fetch(geocodingUrl);
-          const data = await response.json();
-
-          if (data.results && data.results.length > 0) {
-            const { lat, lng } = data.results[0].geometry.location;
-            return redirect(
-              `/?lat=${encodeURIComponent(lat)}&lon=${encodeURIComponent(lng)}`
-            );
-          }
-        } catch (error) {
-          console.error("Geocoding error:", error);
-        }
+          break;
+        default:
+          Error("Invalid locationData type");
       }
-      return json({ success: true });
+
+      const response = await fetch(geocodingUrl);
+      const data = await response.json();
+      if (data.status === "OK") {
+        return redirect(
+          `/?lat=${encodeURIComponent(
+            data.results[0].geometry.location.lat
+          )}&lon=${encodeURIComponent(
+            data.results[0].geometry.location.lng
+          )}&city=${encodeURIComponent(data.results[0].formatted_address)}`
+        );
+      } else {
+        console.error("No geocoding results found")
+        return {error: "No results found"}
+      }
+      
     } else {
       return json({ error: "Invalid location data format" }, { status: 400 });
     }
@@ -70,11 +76,12 @@ export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const lat = url.searchParams.get("lat");
   const lon = url.searchParams.get("lon");
-  return !lat || !lon ? null : { lat: parseFloat(lat), lon: parseFloat(lon) };
+  const city = url.searchParams.get("city");
+  return !lat || !lon ? null : { lat: parseFloat(lat), lon: parseFloat(lon), city: String(city) };
 };
 
 export default function Sunwatch() {
-  const locationData = useLoaderData<{ lat: number; lon: number } | null>();
+  const locationData = useLoaderData<{ lat: number; lon: number, city: string } | null>();
   return (
     <div className={"w-screen min-h-screen bg-gray-950"}>
       <div>
